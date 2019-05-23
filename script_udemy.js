e310377d0696e08cd5aa40d4dba3f6acb0d113ab
Kenny <semen.guryev@ya.ru>
2019-05-23 17:10:27 +0300

    first commit

---
+++ script_udemy.js
@@ -0,0 +1,30 @@
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// let person = {
+//     name: 'Tanya',
+//     age: 31,
+//     isMarried: false
+// };
+
+// console.log(person.name);
+
+// console.log(person["isMarried"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// // console.log(arr[0])
+
+// // alert('go');
+// //let answer = prompt('are you sure?', 'Yes');
+// console.log(typeof(arr));
+// console.log('arr' + ' - object')
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(++incr);
+console.log(decr--);
+console.log(decr);
+alert( "1"[0] );
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+confirm('are you sure?');
